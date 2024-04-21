e4d95d10dc6cb18bc1bc17c7db2dc0cff17191b8
Vladimir Lefterov <https://github.com/hanceanic>
2024-04-21 03:56:51 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:reorderable_grid_view/reorderable_grid_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class ShoppingCartButton extends StatefulWidget {
+  const ShoppingCartButton({super.key});
+
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              isExpanded = !isExpanded;
+            });
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            width: isExpanded ? 200.0 : 80.0,
+            height: isExpanded ? 200.0 : 80.0,
+            decoration: BoxDecoration(
+              color: isExpanded ? Colors.blue : Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 3.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final data = List.generate(25, (i) => i);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget buildItem(String text) {
+      return ShoppingCartButton(
+        key: ValueKey(text),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("<AppBar>"),
+      ),
+      body: Center(
+        child: ReorderableGridView.count(
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 3,
+          children: this.data.map((e) => buildItem("$e")).toList(),
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              final element = data.removeAt(oldIndex);
+              data.insert(newIndex, element);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
